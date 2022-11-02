74d83e60461ed1622f10c8da3cb844d47f74d832
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-11-01 21:07:10 -0300

    create db structure

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 import Fastify from "fastify"
 import cors from '@fastify/cors'
+import ShortUniqueId from 'short-unique-id'
+import { z } from 'zod'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient({
@@ -19,6 +21,38 @@ async function bootstrap() {
         const poolCount = await prisma.pool.count()
         
         return { count: poolCount }
+    })
+
+    fastify.get('/users/count', async () => {
+        const userCount = await prisma.user.count()
+        
+        return { count: userCount }
+    })
+
+    fastify.get('/guesses/count', async () => {
+        const guessesCount = await prisma.guess.count()
+        
+        return { count: guessesCount }
+    })
+
+    fastify.post('/pools', async (request, reply) => {
+        const createPoolBody = z.object({
+            title: z.string(),
+        })
+
+        const { title } = createPoolBody.parse(request.body)
+
+        const generateCode = new ShortUniqueId({ length: 6 })
+        const code = String(generateCode()).toUpperCase()
+
+        await prisma.pool.create({
+            data: {
+                title,
+                code,
+            }
+        })
+        
+        return reply.status(201).send({ code })
     })
 
     await fastify.listen({ port: 3333, host: '0.0.0.0' })
