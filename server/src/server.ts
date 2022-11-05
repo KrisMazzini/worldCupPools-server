478182cb0f52481e0957858127d23706e0f714da
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2022-11-05 10:54:35 -0300

    separate routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,11 @@
 import Fastify from "fastify"
 import cors from '@fastify/cors'
-import ShortUniqueId from 'short-unique-id'
-import { z } from 'zod'
-import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient({
-    log: ['query'],
-})
+import { authRoutes } from "./routes/auth"
+import { gameRoutes } from "./routes/game"
+import { guessRoutes } from "./routes/guess"
+import { poolRoutes } from "./routes/pool"
+import { userRoutes } from "./routes/user"
 
 async function bootstrap() {
     const fastify = Fastify({
@@ -17,43 +16,11 @@ async function bootstrap() {
         origin: true
     })
 
-    fastify.get('/pools/count', async () => {
-        const poolCount = await prisma.pool.count()
-        
-        return { count: poolCount }
-    })
-
-    fastify.get('/users/count', async () => {
-        const userCount = await prisma.user.count()
-        
-        return { count: userCount }
-    })
-
-    fastify.get('/guesses/count', async () => {
-        const guessesCount = await prisma.guess.count()
-        
-        return { count: guessesCount }
-    })
-
-    fastify.post('/pools', async (request, reply) => {
-        const createPoolBody = z.object({
-            title: z.string(),
-        })
-
-        const { title } = createPoolBody.parse(request.body)
-
-        const generateCode = new ShortUniqueId({ length: 6 })
-        const code = String(generateCode()).toUpperCase()
-
-        await prisma.pool.create({
-            data: {
-                title,
-                code,
-            }
-        })
-        
-        return reply.status(201).send({ code })
-    })
+    await fastify.register(authRoutes)
+    await fastify.register(gameRoutes)
+    await fastify.register(guessRoutes)
+    await fastify.register(poolRoutes)
+    await fastify.register(userRoutes)
 
     await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
